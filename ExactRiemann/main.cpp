20111e9c3bbba2ccf3bf507cd1e1f7783d6db78e
MatejVitovec <Matej.Vitovec@fs.cvut.cz>
2022-11-05 17:42:36 +0100

    dokonceni vypoctu star regionu

--- ExactRiemann/main.cpp
+++ ExactRiemann/main.cpp
@@ -5,38 +5,98 @@
 
 struct State
 {
-    double density;
-    double velocity;
-    double pressure;
+    double d;
+    double u;
+    double p;
 };
 
-struct pressureFunctionMembers
+struct PressureFunctionMembers
 {
     double f;
     double fd;
 };
 
-double gamma;
+double GAMMA;
 
-void starPressureVelocity(State stateL, State stateR)
+double soundSpeed(State state)
 {
-    int nIter;
-    double pressureTolerance;
-    double startPressure;
-
-    startPressure = 0.1;
-
-
-
+    return sqrt((GAMMA*state.p)/state.d);
 }
 
-pressureFunctionMembers pressureFunction()
+PressureFunctionMembers pressureFunction(State stateK, double p)
 {
-    pressureFunctionMembers out;
+    PressureFunctionMembers out;
 
+    if(p <= stateK.p)
+    {
+        //Refraction wave
+        double prat = p/stateK.p;
+        double ck = soundSpeed(stateK);
+        out.f = (2.0/(GAMMA - 1.0))*ck*(pow(prat, (GAMMA - 1.0)/(2*GAMMA)) - 1.0);
+        out.fd = (1.0/(stateK.d*ck))*pow(prat, -((GAMMA + 1.0)/(2.0*GAMMA)));
+    }
+    else
+    {
+        // Shock wave
+        double ak = (2.0/(GAMMA + 1.0))/stateK.d;
+        double bk = ((GAMMA - 1.0)/(GAMMA+1.0))*stateK.p;
+        double qrt = sqrt(ak/(bk + p));
+        out.f = (p - stateK.p)*qrt;
+        out.fd = (1 - (0.5*(p - stateK.p))/(bk + p))*qrt;
+    }
 
     return out;
 }
+
+State starPressureVelocity(State stateL, State stateR)
+{
+    State out;
+    int nIter = 0;
+    double pTolerance;
+    double startPressure;
+
+    pTolerance = 1.0e-6;
+    startPressure = 0.2;
+
+    double p;
+    double pOld = startPressure;
+    double u;
+    double uDiff = stateR.u - stateL.u;
+
+    double change;
+
+    PressureFunctionMembers prefunL;
+    PressureFunctionMembers prefunR;
+
+    while(1)
+    {
+        nIter++;
+        prefunL = pressureFunction(stateL, pOld);
+        prefunR = pressureFunction(stateR, pOld);
+        p = pOld - (prefunL.f + prefunR.f + uDiff)/(prefunL.fd + prefunR.fd);
+        change = 2.0*fabs((p - pOld)/(p + pOld));
+
+        if (change <= pTolerance)
+        {
+            break;
+        }
+        if (p <= 0)
+        {
+            p = pTolerance;
+        }
+
+        pOld = p;
+    }
+
+    u = 0.5*(stateL.u + stateR.u + prefunR.f - prefunL.f);
+
+    out.p = p;
+    out.u = u;
+
+    return out;
+}
+
+
 
 int main()
 {
@@ -50,20 +110,19 @@ int main()
     State stateL;
     State stateR;
 
-    gamma = 1.4;
-
-    domainLenght = 1;
+    GAMMA = 1.4;
+    domainLenght = 1.0;
     initialDiscontinuityPosition = 0.5;
     time = 0.25;
 
-    stateL.density = 0;    
-    stateL.pressure = 0;
-    stateL.velocity = 0;
-    stateR.density = 0;
-    stateR.pressure = 0;
-    stateR.velocity = 0;
-
-    //sqrt((gamma*initStates.pressureL)/initStates.densityL);
+    stateL.d = 1.0;
+    stateL.u = 0.0;
+    stateL.p = 1.0;    
+    stateR.d = 0.125;
+    stateR.u = 0.0;
+    stateR.p = 0.1;
+    
+    State starRegionPU = starPressureVelocity(stateL, stateR);
 
 
 
