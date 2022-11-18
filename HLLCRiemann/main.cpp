605b9769d2a82cce4a122117c7369d5a404e8128
MatejVitovec <Matej.Vitovec@fs.cvut.cz>
2022-11-18 22:11:43 +0100

    pridani HLL vypoctu - funguje ale spatne

--- HLLCRiemann/main.cpp
+++ HLLCRiemann/main.cpp
@@ -1,4 +1,3 @@
-//test
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -9,66 +8,6 @@
 enum {RHO, RHO_U, RHO_E};
 
 double GAMMA;
-
-void loadData(std::string fileName, double& domlen, double& diaph, double& cells, double& lGamma, double& time, Vector3& stateL, Vector3& stateR)
-{
-    std::ifstream file(fileName);
-    std::string line;
-
-    std::getline(file, line);
-    domlen = std::stod(line);
-    std::getline(file, line);
-    diaph = std::stod(line);
-    std::getline(file, line);
-    cells = std::stod(line);
-    std::getline(file, line);
-    lGamma = std::stod(line);
-    std::getline(file, line);
-    time = std::stod(line);
-
-    std::getline(file, line);
-    stateL[0] = std::stod(line);
-    std::getline(file, line);
-    stateL[1] = std::stod(line);
-    std::getline(file, line);
-    stateL[2] = std::stod(line);
-
-    std::getline(file, line);
-    stateR[0] = std::stod(line);
-    std::getline(file, line);
-    stateR[1] = std::stod(line);
-    std::getline(file, line);
-    stateR[2] = std::stod(line);
-
-    file.close();
-}
-
-void saveData(std::string fileName, std::vector<std::vector<double>> data, double domLen, double cells, double time)
-{
-    std::ofstream writeToFile(fileName);
-    writeToFile << domLen << std::endl;
-    writeToFile << cells << std::endl;
-    writeToFile << time << std::endl;
-
-    for (int j = 0; j < data.size(); j++)
-    {
-        for (int i = 0; i < data[j].size(); i++)
-        {
-            if(i == data[j].size() - 1)
-            {
-                writeToFile << data[j][i];
-            }
-            else
-            {
-                writeToFile << data[j][i] << ",";
-            }
-        }
-        writeToFile << std::endl;
-    }
-    
-    writeToFile.close();
-}
-
 
 inline double density(const Vector3& w)
 {
@@ -123,11 +62,196 @@ inline Vector3 flux(const Vector3& w)
     return u * w + Vector3({0.0, p, p*u});
 }
 
-
-
-
-main()
+Vector3 primitiveToConservative(Vector3 v)
 {
+    return Vector3({v[0], v[0]*v[1], (v[2]/((GAMMA - 1.0)*v[0]))*v[0]});
+}
+
+
+Vector3 riemannInitialCondition(double x, Vector3 stateL, Vector3 stateR, double initDiscontinuityPos)
+{
+    if (x < initDiscontinuityPos)
+    {
+        return stateL;
+    }
+    else
+    {
+        return stateR;
+    }
+}
+
+double maxTimeStep(const std::vector<Vector3>& w, double dx)
+{
+    double dt = 10e+100;
+
+    for (int i = 0; i < w.size(); i++)
+    {
+        dt = std::min(dt, dx/spectralRadius(w[i]));
+    }
+
+    return dt;
+}
+
+Vector3 HLL(const Vector3& wl, const Vector3& wr)
+{
+    Vector3 lambdaL = waveSpeeds(wl);
+    Vector3 lambdaR = waveSpeeds(wr);
+
+    double sl = std::min(lambdaL[0], lambdaR[0]);
+    double sr = std::max(lambdaL[2], lambdaR[2]);
+
+    if (0 <= sl)
+    {
+        return flux(wl);
+    }
+    else if (0 < sr)
+    {
+        return (sr*flux(wl) - sl*flux(wr) + sl*sr*(wr - wl)) / (sr - sl);
+    }
+    else
+    {
+        return flux(wr);
+    }
+}
+
+Vector3 HLLC(const Vector3& wl, const Vector3& wr)
+{
+    //TODO
+
+    return Vector3();
+}
+
+void loadData(std::string fileName, double& domlen, double& diaph, double& cells, double& lGamma, double& time, Vector3& stateL, Vector3& stateR)
+{
+    std::ifstream file(fileName);
+    std::string line;
+
+    std::getline(file, line);
+    domlen = std::stod(line);
+    std::getline(file, line);
+    diaph = std::stod(line);
+    std::getline(file, line);
+    cells = std::stod(line);
+    std::getline(file, line);
+    lGamma = std::stod(line);
+    std::getline(file, line);
+    time = std::stod(line);
+
+    std::getline(file, line);
+    stateL[0] = std::stod(line);
+    std::getline(file, line);
+    stateL[1] = std::stod(line);
+    std::getline(file, line);
+    stateL[2] = std::stod(line);
+
+    std::getline(file, line);
+    stateR[0] = std::stod(line);
+    std::getline(file, line);
+    stateR[1] = std::stod(line);
+    std::getline(file, line);
+    stateR[2] = std::stod(line);
+
+    file.close();
+}
+
+void saveData(std::string fileName, std::vector<Vector3> w, double domLen, double cells, double time)
+{
+    std::ofstream writeToFile(fileName);
+    writeToFile << domLen << std::endl;
+    writeToFile << cells << std::endl;
+    writeToFile << time << std::endl;
+
+    for (int i = 0; i < w.size(); i++)
+    {
+        writeToFile << density(w[i]) << ","<< velocity(w[i]) << "," << pressure(w[i]) << "," << totalEnergy(w[i]) << std::endl;
+    }
     
+    writeToFile.close();
+}
+
+
+int main(int argc, char** argv)
+{
+    std::string inputFileName = "data.txt";
+    std::string outputFileName = "results/result.txt";
+
+
+    if(argc == 3)
+    {
+        inputFileName = argv[1];
+        outputFileName = argv[2];
+    }
+
+    double domainLenght;
+    double initDiscontinuityPos;
+    double n;
+    double setTime;
+    Vector3 stateL;
+    Vector3 stateR;
+
+    loadData(inputFileName, domainLenght, initDiscontinuityPos, n, GAMMA, setTime, stateL, stateR);
+
+
+    double dx = domainLenght/n;
+    double time = 0;
+
+    Vector3 wl = primitiveToConservative(stateL);
+    Vector3 wr = primitiveToConservative(stateR);
+
+    std::vector<Vector3> w(n);
+    std::vector<Vector3> wn(n);
+    std::vector<Vector3> f(n + 1);
+
+    for (int i = 0; i < w.size(); i++)
+    {
+        w[i] = riemannInitialCondition((i + 0.5)*dx, wl, wr, initDiscontinuityPos);
+    }
+
+    bool exitCalcualtion = false;
+    int iter = 0;
+
+    //saveData(outputFileName, w, domainLenght, n, setTime);
+
+    while (1)
+    {
+        
+        f[0] = HLL(w[0], w[0]);
+        f[n] = HLL(w[n-1], w[n-1]);
+
+        for (int i = 1; i < n; i++)
+        {
+            f[i] = HLL(w[i-1], w[i]);
+        }
+
+        double dt = 0.4 * maxTimeStep(w, dx);
+        time += dt;
+        if(setTime - time < dt)
+        {
+            dt = setTime - time;
+            exitCalcualtion = true;
+        }
+
+
+        for (int i = 0; i < n; i++)
+        {
+            wn[i] = w[i] - dt/dx * (f[i+1] - f[i]);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            w[i] = wn[i];
+        }
+
+        iter++;
+
+        if(exitCalcualtion)
+        {
+            time += dt;
+            break;
+        }
+    }
+
+    saveData(outputFileName, w, domainLenght, n, setTime);
+
     return 0;
 }
---
+++ HLLCRiemann/plotRiemann.py
@@ -0,0 +1,67 @@
+import sys
+from matplotlib import pyplot as plt
+
+inputFileName = "results/result.txt"
+outputFileName = "results/result.png"
+
+if len(sys.argv) == 3:
+    inputFileName = sys.argv[1]
+    outputFileName = sys.argv[2]
+
+f = open(inputFileName, "r")
+
+domLen = float(f.readline())
+cells = int(f.readline())
+time = float(f.readline())
+
+dx = domLen/cells
+
+d = []
+u = []
+p = []
+e = []
+x = []
+
+for line in f:
+    aux = line.split(",")
+    d.append(float(aux[0]))
+    u.append(float(aux[1]))
+    p.append(float(aux[2]))
+    e.append(float(aux[3]))
+
+f.close()
+
+for i in range(0, cells):
+    x.append(i*dx)
+    i = i + 1
+
+
+plt.figure()
+
+plt.subplot(2,2,1)
+plt.plot(x,d)
+plt.xlabel("Position")
+plt.ylabel("Density")
+
+plt.subplot(2,2,2)
+plt.plot(x,u)
+plt.xlabel("Position")
+plt.ylabel("Velocity")
+
+plt.subplot(2,2,3)
+plt.plot(x,p)
+plt.xlabel("Position")
+plt.ylabel("Pressure")
+
+plt.subplot(2,2,4)
+plt.plot(x,e)
+plt.xlabel("Position")
+plt.ylabel("Internal energy")
+
+plt.suptitle("t = " + str(time))
+
+plt.tight_layout()
+
+plt.savefig(outputFileName)
+
+plt.close()
